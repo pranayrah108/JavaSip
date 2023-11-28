d6cdf94d88a2b3b8f08760def0900795cdb1567b
pranay <pranayrahangdale23@gmail.com>
2023-11-28 21:30:08 +0530

    ksw

--- Patterns-ksw/src/com/code/Main.java
+++ Patterns-ksw/src/com/code/Main.java
@@ -4,11 +4,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		pattern1(6);
-		pattern2(5);
+//		pattern1(6);
+//		pattern2(5);
+		pattern3(5);
 
 	}
 	
+	
+	static void pattern3(int n) {
+		for(int row=1;row<=n;row++) {
+			for(int col =1;col<= n-row+1;col++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
 	
 	
 	static void pattern2(int n) {
