0fd90a0bb9e65afcdde11a4ae23a1ae9e55e0bed
pranayrah108 <126639580+pranayrah108@users.noreply.github.com>
2024-02-06 17:53:33 +0530

    Delete day14_lab directory

--- day14_lab/src/main/java/com/app/dao/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Employee;
-
-public interface EmployeeDao extends JpaRepository<Employee, Long> {
-
-}
